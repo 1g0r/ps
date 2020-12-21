ec99d934d8a3d3da8309ecd0e9bf36d401f7ec94
1g0r <logovskoy@gmail.com>
2020-12-21 23:42:54 +0300

    Update open.ps1

--- rt/lib/open.ps1
+++ rt/lib/open.ps1
@@ -1,1 +1,24 @@
 Using module ..\..\apps\cmdbase.psm1
+
+class OpenCommand : CommandBase {
+  hidden [string] $ide
+  hidden [System.Collections.Hashtable] $solutions
+  
+  hidden [ScriptBlock] $script = {
+    param([OpenCommand]$self, [string]$solution)
+    
+    $path = $self.solutions[$solution];
+    & $self.ide $path
+  }
+  
+  OpenCommand($config) : base(
+    @(
+      [ParameterBase]::new("solution", 1, $true, $config.solutions.Keys)
+    ),
+    $this.script
+  )
+  {
+    $this.ide = $config.ide;
+    $this.solutions = $config.solutions;
+  }
+}
