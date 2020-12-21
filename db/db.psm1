36a180bb01f647496d1a3d2a2fb9e3348fc03773
1g0r <logovskoy@gmail.com>
2020-12-21 17:33:52 +0300

    Update db.psm1

--- db/db.psm1
+++ db/db.psm1
@@ -1,3 +1,13 @@
 # Uncomment to run script manually
 #$PSScriptRoot = 'c:\....'
 . $PSScriptRoot\lib\dreq.ps1
+
+$dbConfigPath = Join-Path $PSScriptRoot '\config\db.json'
+$config = (Get-Content $dbConfigPath) -join "`n" | ConvertFrom-Json | Convert-ToHashTable;
+
+$commands = @{
+  delRequest = [DelRequestCommand]::new($config)
+};
+
+Create-Application 'db' $commands
+Export-ModuleMember db
