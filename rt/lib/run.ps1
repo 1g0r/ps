8b89a276bdd038c78314734a1b5b54318a30437e
1g0r <logovskoy@gmail.com>
2020-12-22 10:33:08 +0300

    Update run.ps1

--- rt/lib/run.ps1
+++ rt/lib/run.ps1
@@ -48,5 +48,39 @@ class RunCommand : CommandBase {
   hidden $projectsToRun = @{
     all = @('proj1', 'proj2', 'proj3');
     
+    proj1 = {
+      param([string]#slnConfig, [switch]$stop)
+      
+      if (-not $stop.IsPresent) {
+        $this.checkConfig($slnConfig)
+      }
+      gps proj1 -ErrorAction SilentlyContinue | Kill -ErrorAction SilentlyContinue;
+      if (-not $stop.IsPresent) {
+        Write-Host "c:\..\bin\$slnConfig\proj1.exe"
+        Start-Process -FilePath "c:\..\bin\$slnConfig\proj1.exe" -WindowStyle Minimized
+      }
+    };
+    
+    webApi = {
+      param ([string]$slnConfig, [switch]$stop)
+      
+      gps iisexpress -ErrorAction SilentlyContinue | Kill -ErrorAction SilentlyContinue;
+      if ($stop.IsPresent) {
+        return;
+      }
+      $this.checkConfig($slnConfig)
+      $deleted = [CommandBase]::removeFolder("$env:USERPROFILE\AppData\Local\Temp\Temporary ASP.NET Files\", $this.shouldDelete, $false);
+      if ($deleted -ne 0) {
+        Write-Host "$deleted folders were found" -ForegroundColor Yellow
+      }
+      if (-not $stop.IsPresent) {
+        Start-Process 'c:\Program Files\IIS Express\iisexpress.exe' '/port:8080 /path:c:\...\ /trace:i' -WindowStyle Hidden 
+      }
+    };
+    
+    w3svc = {
+      Set-Service -Name W3SVC -StartupType Automatic
+      Start-Service -Name W3SVC
+    };
   }
 }
