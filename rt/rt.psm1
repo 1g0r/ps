ab6c87d495cd1610dae5674be4ff580faf51076f
1g0r <logovskoy@gmail.com>
2020-12-22 10:41:34 +0300

    Create rt.psm1

---
+++ rt/rt.psm1
@@ -0,0 +1,11 @@
+#Uncomment to run script manually
+#$PSScriptRoot = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\rt"
+. $PSScriptRoot\lib\branch.ps1
+. $PSScriptRoot\lib\build.ps1
+. $PSScriptRoot\lib\clear.ps1
+. $PSScriptRoot\lib\delete.ps1
+. $PSScriptRoot\lib\merge.ps1
+. $PSScriptRoot\lib\
+. $PSScriptRoot\lib\
+. $PSScriptRoot\lib\
+. $PSScriptRoot\lib\
