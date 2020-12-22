e0ad7c85fe8686d86ceb6be1e8845df7a6a76ce7
1g0r <logovskoy@gmail.com>
2020-12-22 10:49:25 +0300

    Update rt.psm1

--- rt/rt.psm1
+++ rt/rt.psm1
@@ -5,7 +5,26 @@
 . $PSScriptRoot\lib\clear.ps1
 . $PSScriptRoot\lib\delete.ps1
 . $PSScriptRoot\lib\merge.ps1
-. $PSScriptRoot\lib\
-. $PSScriptRoot\lib\
-. $PSScriptRoot\lib\
-. $PSScriptRoot\lib\
+. $PSScriptRoot\lib\open.ps1
+. $PSScriptRoot\lib\restore.ps1
+. $PSScriptRoot\lib\run.ps1
+. $PSScriptRoot\lib\work.ps1
+
+$rtConfigPath = Join-Path $PSScriptRoot '\config\rt.json';
+$config = (Get-Content $rtConfigPath) -join "`n" | ConvertFrom-Json | ConvertTo-HashTable;
+
+$build = [BuildCommand]::new($config);
+$commands = @{
+  branch = [BranchCommand]::new($config);
+  build = $build;
+  clear = [ClearCommand]::new($config, $build.clearConfig);
+  delete = [DeleteCommand]::new($config);
+  merge = [MergeCommand]::new($config);
+  open = [OpenCommand]::new($config);
+  restore = [RestoreCommand]::new($config);
+  run = [RunCommand]::new($config, $build.getConfig);
+  work = [WorkCommand]::new($config);
+};
+
+Create-Application 'rt' $commands
+Export-ModuleMember rt
