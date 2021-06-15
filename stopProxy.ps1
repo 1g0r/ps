0f90f7f7f848c319b1cd6d28cc9c37ff24585acc
1g0r <logovskoy@gmail.com>
2021-06-15 22:02:30 +0300

    Create stopProxy.ps1

---
+++ stopProxy.ps1
@@ -0,0 +1,16 @@
+[ScriptBlock]$disableProxy = {
+  $regKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+  while($true) {
+    $enabled = Get-ItemPropertyValue -Path $regKey -Name ProxyEnable
+    if ($enabled -eq 1) {
+      Set-ItemProperty -path $regKey ProxyEnable -value 0
+      Write-Host 'System proxy have been Disabled!'
+    } else {
+      Write-Host 'System proxy is Disabled'
+    }
+  }
+}
+
+$jobName = 'StopProxyJob'
+
+Start-Job -Name $jobName -ScriptBlock $disableProxy
