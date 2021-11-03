4cace3b56a66c4b2a2925256506eb857b7c12eb5
1g0r <logovskoy@gmail.com>
2021-11-03 11:21:40 +0300

    Bug fixes

--- apps/apps.psm1
+++ apps/apps.psm1
@@ -1,83 +1,83 @@
 function toString($arr) {
-  $result = ""
-  $arr | % {
-    $result += "'$_', "
-  }
-  return $result.Remove($result.Length - 2, 2);
+    $result = ""
+    $arr | % {
+        $result += "'$_', "
+    }
+    return $result.Remove($result.Length - 2, 2);
 }
 
 function ConvertFrom-PSObject {
-  [CmdletBinding()]
-  Param(
-    [Parameter(Mandatory=$true,
-               ValueFromPipeline=$true,
-               ValueFromPipelineByPropertyName=$true,
-               Position=0)]
-    $object
-  )
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,
+                ValueFromPipeline=$true,
+                ValueFromPipelineByPropertyName=$true,
+                Position=0)]
+        $object
+    )
 
-  $type = $object.GetType()
-  if($type.Name.Equals('PSCustomObject', [System.StringCompartion]::InvariantCultureIgnoreCase)) {
-    $result = @{};
-    $object.psobject.properties | % {$result[$_.name] = (ConvertFrom-PSObject $_.value)}
-    return $result;
-  }
-  if ($type.IsArray) {
-    $arr = @();
-    $object | % { $arr += ConvertFrom-PSObject $_ }
-    return $arr;
-  }
-  return $object;
+    $type = $object.GetType()
+    if($type.Name.Equals('PSCustomObject', [System.StringCompartion]::InvariantCultureIgnoreCase)) {
+        $result = @{};
+        $object.psobject.properties | % { $result[$_.name] = (ConvertFrom-PSObject $_.value) }
+        return $result;
+    }
+    if ($type.IsArray) {
+        $arr = @();
+        $object | % { $arr += ConvertFrom-PSObject $_ }
+        return $arr;
+    }
+    return $object;
 }
 
 function Create-Application {
-  [CmdletBinding()]
-  param(
-    [Parameter(Position=0, Manatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [string] $name,
-    [Parameter(Position=1, Mandatory=$true)]
-    [Hashtable]$commands
-  )
-  begin {
-    if ([string]::IsNullOrWhiteSpace($name)) {
-      throw "Name ot the application can not be empty!"
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, Manatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $name,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [Hashtable]$commands
+    )
+    begin {
+        if ([string]::IsNullOrWhiteSpace($name)) {
+            throw "Name ot the application can not be empty!"
+        }
+        if ($null -eq $commands -or $commands.Keys.Count -eq 0) {
+            throw "Commands of the application $name can not be empty!"
+        }
     }
-    if ($null -eq $commands -or $commands.Keys.Count -eq 0) {
-      throw "Commands of the application $name can not be empty!"
-    }
-  }
-  process {
-    $componentSet = toString($commands.Keys)
-    $closure = {
-      $body = @"
+    process {
+        $componentSet = toString($commands.Keys)
+        $closure = {
+            $body = @"
 function global:$name {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory='$true, Position=0)]
-    [ValidateSet($componentSet)]
-    [string]`$__name_of_command
-  )
-  DynamicParam {
-    if (`$commands.Contains(`$__name_of_command)) {
-      `$params = `$commands[`$__name_of_command].BuildParameters()
-      return `$params
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory='$true', Position=0)]
+        [ValidateSet($componentSet)]
+        [string]`$__name_of_command
+    )
+    DynamicParam {
+        if (`$commands.Contains(`$__name_of_command)) {
+            `$params = `$commands[`$__name_of_command].BuildParameters()
+            return `$params
+        }
     }
-  }
-  begin {
-    `$command = `$commands[`$__name_of_command];
-  }
-  process {
-    `$tmp = `$PSBoundParameters.Remove('__name_of_command');
-    `$command.Invoke(`$PSBoundParameters) | Out-Default
-  }
+    begin {
+        `$command = `$commands[`$__name_of_command];
+    }
+    process {
+        `$tmp = `$PSBoundParameters.Remove('__name_of_command');
+        `$command.Invoke(`$PSBoundParameters) | Out-Default
+    }
 }
 "@
-      #Write-Host $body
-      Invoke-Expression $body
-    }.GetNewCosure()
-    & $closure
-  }
+            #Write-Host $body
+            Invoke-Expression $body
+        }.GetNewClosure();
+        & $closure
+    }
 }
 
 Export-ModuleMember Create-Application
--- apps/cmdbase.psm1
+++ apps/cmdbase.psm1
@@ -1,193 +1,204 @@
-class ParameterBase{
-  [string] $name;
-  [Type] $type;
-  [int] $position;
-  [bool] $mandatory;
-  [string[]] $validateSet;
-  [ScriptBlock] $getValidateSet;
-  
-  hidden [void] setFields(
-    [string]$name, 
-    [int]$position, 
-    [bool]$mandatory, 
-    [string[]]$validateSet=$null,
-    [ScriptBlock]$getValidateSet=$null,
-    [Type]$type)
-  {
-    if ([string]::IsNullOrEmpty($name)){
-      throw "Parameter must have name!"
-    }
-    $this.name = $name;
+class ParameterBase {
+    [string] $name;
+    [Type] $type;
+    [int] $position;
+    [bool] $mandatory;
+    [string[]] $validateSet;
+    [ScriptBlock] $getValidateSet;
+    $defaultValue;
 
-    if ($null -eq $position -or $position -eq 0){
-      $this.position = 1
-    } else {
-      $this.position = $position
+    hidden [void] setFields(
+        [string]$name, 
+        [int]$position, 
+        [bool]$mandatory, 
+        [string[]]$validateSet = $null,
+        [ScriptBlock]$getValidateSet = $null,
+        [Type]$type) {
+        if ([string]::IsNullOrEmpty($name)) {
+            throw "Parameter must have name!"
+        }
+        $this.name = $name;
+
+        if ($null -eq $position -or $position -eq 0) {
+            $this.position = 1
+        } else {
+            $this.position = $position
+        }
+
+        $this.mandatory = if ($null -ne $mandatory) { $mandatory } else { $false }
+        $this.validateSet = $validateSet
+        $this.getValidateSet = $getValidateSet
+        $this.type = if ($null -eq $type) { [string] } else { $type }
+        if ($this.type -eq [bool]) {
+            $this.defaultValue = $false;
+        } elseif ($this.type -eq [datetime]) {
+            $this.defaultValue = Get-Date;
+        } else {
+            $this.defaultValue = $null;
+        }
     }
 
-    $this.mandatory = if ($null -ne $mandatory) { $mandatory } else { $false }
-    $this.validateSet = $validateSet
-    $this.getValidateSet = $getValidateSet
-    $this.type = if ($null -eq $type) { [string] } else { $type }
-  }
-  
-  ParameterBase([string]$name){
-    $this.setFields($name, 1, $false, $null, $null, [string])
-  }
-  
-  ParameterBase([string]$name, [int]$position, [bool]$mandatory){
-    $this.setFields($name, $position, $mandatory, $null, $null, [string])
-  }
-  
-  ParameterBase([string]$name, [int]$position, [bool]$mandatory, [string[]]$validateSet){
-    $this.setFields($name, $position, $mandatory, $validateSet, $null, [string])
-  }
-  
-  ParameterBase([string]$name, [int]$position, [bool]$mandatory, [string[]]$validateSet, [type]$type) {
-    $this.setFields($name, $position, $mandatory, $validateSet, $null, $type);
-  }
-
-  ParameterBase([string]$name, [int]$position, [bool]$mandatory, [ScriptBlock]$getValidateSet){
-    $this.setFields($name, $position, $mandatory, $null, $getValidateSet, [string])
-  }
-  
-  ParameterBase([string]$name, [int]$position, [bool]$mandatory, [Type]$type){
-    $this.setFields($name, $position, $mandatory, $null, $null, $type)
-  }
-  
-  [System.Management.Automation.RunTimeDefinedParameter] Build([string] $paramName) {
-    $attributeCollection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
-    
-    $attribute = New-Object System.Management.Automation.ParameterAttribute;
-    $attribute.Position = $this.position;
-    $attribute.Mandatory = $this.mandatory;
-    $attributeCollection.Add($attribute)
-    
-    if ($null -ne $this.validateSet -and $this.validateSet.Lengh -ge 0) {
-      $attribute = New-Object System.Management.Automation.ValidateSetAttribute($this.validateSet)
-      $attributeCollection.Add($attribute)
+    ParameterBase([string]$name) {
+        $this.setFields($name, 1, $false, $null, $null, [string]);
     }
-    if ($null -ne $this.getValidateSet) {
-      $setOfValues = Invoke-Command -ScriptBlock $this.getValidateSet
-      if ($null -ne $setOfValues -and $setOfValues.Lengh -ge 0) {
-        $attribute = New-Object System.Management.Automation.ValidateSetAttribute($setOfValues)
+
+    ParameterBase([string]$name, [int]$position, [bool]$mandatory) {
+        $this.setFields($name, $position, $mandatory, $null, $null, [string]);
+    }
+
+    ParameterBase([string]$name, [int]$position, [bool]$mandatory, [string[]]$validateSet) {
+        $this.setFields($name, $position, $mandatory, $validateSet, $null, [string]);
+    }
+
+    ParameterBase([string]$name, [int]$position, [bool]$mandatory, [string[]]$validateSet, [type]$type) {
+        $this.setFields($name, $position, $mandatory, $validateSet, $null, $type);
+    }
+
+    ParameterBase([string]$name, [int]$position, [bool]$mandatory, [ScriptBlock]$getValidateSet) {
+        $this.setFields($name, $position, $mandatory, $null, $getValidateSet, [string]);
+    }
+
+    ParameterBase([string]$name, [int]$position, [bool]$mandatory, [Type]$type) {
+        $this.setFields($name, $position, $mandatory, $null, $null, $type);
+    }
+
+    [System.Management.Automation.RunTimeDefinedParameter] Build([string] $paramName) {
+        $attributeCollection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+
+        $attribute = New-Object System.Management.Automation.ParameterAttribute;
+        $attribute.Position = $this.position;
+        $attribute.Mandatory = $this.mandatory;
         $attributeCollection.Add($attribute)
-      }
+
+        if ($null -ne $this.validateSet -and $this.validateSet.Lengh -ge 0) {
+            $attribute = New-Object System.Management.Automation.ValidateSetAttribute($this.validateSet)
+            $attributeCollection.Add($attribute)
+        }
+        if ($null -ne $this.getValidateSet) {
+            $setOfValues = Invoke-Command -ScriptBlock $this.getValidateSet
+            if ($null -ne $setOfValues -and $setOfValues.Lengh -ge 0) {
+                $attribute = New-Object System.Management.Automation.ValidateSetAttribute($setOfValues)
+                $attributeCollection.Add($attribute)
+            }
+        }
+        $dynParam = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($this.name, $this.type, $attributeCollection)
+        return $dynParam    
     }
-    $dynParam = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($this.name, $this.type, $attributeCollection)
-    return $dynParam    
-  }
 }
 
-class CommandBase{
-  [ParameterBase[]] $parameters;
-  [ScriptBlock] $body;
-  [hashtable] $solutions;
-  
-  CommandBase([ParameterBase[]]$parameters, [ScriptBlock]$body, [hashtable]$config) {
-    if ($this.shouldAddSlnParameter($parameters, $config)) {
-      $this.parameters = $parameters + @(, [ParameterBase]::new("sln", $parameters.Length + 1, $false, $config.solutions.Keys));
-    }
+class CommandBase {
+    [ParameterBase[]] $parameters;
+    [ScriptBlock] $body;
+    [hashtable] $solutions;
 
-    if ($null -eq $body) {
-      throw "Command body can not be null";
-    }
-    $this.body = $body;
-    $this.solutions = $config.solutions;
-  }
-  
-  [System.Management.Automation.RuntimeDefinedParameterDictionary] BuildParameters() {
-    $result = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
-    foreach ($param in $this.parameters) {
-      $result.Add($param.name, $param.Build($param.name))
-    }
-    return $result;
-  }
-  
-  [void] Invoke([hashtable] $psParams) {
-    $solution = $this.fetchSolution($psParams);
-    if ($null -eq $solution) {
-      Write-Host 'Specify solution explisitly or change directory to solution"s folder' -ForegroundColor Red
-      return;
-    }
-    $argv = @($this, $solution);
-    $paramsMeta = $this.BuildParameters();
-    foreach($key in $paramsMeta.Keys) {
-      if ($psParams.ContainsKey($key)) {
-        $argv += @(, $psParams[$key]);
-      } else {
-        $argv += @(, $null);
-      }
-    }
-    Invoke-Command -ScriptBlock $this.body -ArgumentList $argv | Out-Default
-  }
-  
-  static [string[]] getBranches([string]$repoPath, [string]$user, [bool]$mine = $false) {
-    $curPath = Get-Location;
-    Set-Location $repoPath
-    $result = if ((Test-Path .\.git)) {
-      git branch |
-      Where-Object { if ($mine) { $_.Contains($user) } else { -not $_.Contains($user) } } |
-      ForEach-Object { $_.Trim("*", " ").Replace("$($user)/", "") } |
-      Sort-Object
-    } else {
-      @()
-    }
-    Set-Location $curPath
-    return $result;
-  }
-  
-  static [int] removeFolder([string]$path, [System.Management.Automation.PSMethod]$shouldDelete, [bool]$recurse) {
-    if (-not (Test-Path $path)) {
-      return 0;
-    }
-    $count = 0
-    $items = if ($recurse) { Get-ChildItem $path -Recurse -Directory } else { Get-ChildItem $path -Directory }
-    foreach ($dit in $items) {
-      if ($shouldDelete.Invoke($dir)) {
-        Write-Host $dir.FullName -NoNewline
-        Write-Host ('.' * (120 - $dir.FullName.Length)) -NoNewline
-        rm $dir.FullName -Resurse -Force
-        Write-Host ' DELETED' -ForegroundColor Green
-        $count++
-      }
-    }
-    return $count;
-  }
-
-  hidden [bool] shouldAddSlnParameter([ParameterBase[]] $parameters, [hashtable] $config) {
-    return $null -ne $parameters -and
-      $null -ne $config -and $null -ne $config.solutions -and $config.solutions.count -gt 0;
-  }
-
-  hidden [hashtable] fetchSolution([hashtable] $psParams) {
-    $key = $psParams['sln'];
-    if ([string]::IsNullOrEmpty($key)) {
-      return $this.fetchSolutionFromCurrentDir();
-    }
-    $psParams.Remove($key);
-    $result = $this.solutions[$key];
-    if ($null -ne $result) {
-      $result['key'] = $key;
-    }
-    return $result;
-  }
-
-  hidden [hashtable] fetchSolutionFromCurrentDir() {
-    $sln = if ((Test-Path .\src)) {
-      Get-Item .\src\*.sln
-    } else {
-      Get-Item .\*.sln
-    }
-    if ($null -ne $sln) {
-      $slnRoot = $sln.fullname.ToLower();
-      foreach($key in $this.solutions.Keys) {
-        $solution = $this.solutions[$key];
-        if ($slnRoot -eq $solution.path.ToLower()) {
-          $solution['key'] = $key;
-          return $solution;
+    CommandBase([ParameterBase[]]$parameters, [ScriptBlock]$body, [hashtable]$config) {
+        if ($this.shouldAddSlnParameter($parameters, $config)) {
+            $this.parameters = $parameters + @(, [ParameterBase]::new("sln", $parameters.Length + 1, $false, $config.solutions.Keys));
+        } else {
+            $this.parameters = $parameters;
         }
-      }
+
+        if ($null -eq $body) {
+            throw "Command body can not be null";
+        }
+        $this.body = $body;
+        $this.solutions = $config.solutions;
     }
-  }
+
+    [System.Management.Automation.RuntimeDefinedParameterDictionary] BuildParameters() {
+        $result = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+        foreach ($param in $this.parameters) {
+            $result.Add($param.name, $param.Build($param.name))
+        }
+        return $result;
+    }
+
+    [void] Invoke([hashtable] $psParams) {
+        $argv = @($this);
+        $solution = $this.fetchSolution($psParams);
+        if ($null -ne $solution) {
+            $argv += @(, $solution);
+        }
+
+        $paramsMeta = $this.BuildParameters();
+        foreach($key in $paramsMeta.Keys) {
+            if ($psParams.ContainsKey($key)) {
+                $argv += @(, $psParams[$key]);
+            } else {
+                $defaultValue = $this.parameters | Where-Object {$_.name -eq $key};
+                $argv += @(, $defaultValue.defaultValue);
+            }
+        }
+        Invoke-Command -ScriptBlock $this.body -ArgumentList $argv | Out-Default
+    }
+
+    static [string[]] getBranches([string]$repoPath, [string]$user, [bool]$mine = $false) {
+        $curPath = Get-Location;
+        Set-Location $repoPath
+        $result = if ((Test-Path .\.git)) {
+            git branch |
+            Where-Object { if ($mine) { $_.Contains($user) } else { -not $_.Contains($user) } } |
+            ForEach-Object { $_.Trim("*", " ").Replace("$($user)/", "") } |
+            Sort-Object
+        } else {
+            @()
+        }
+        Set-Location $curPath
+        return $result;
+    }
+
+    static [int] removeFolder([string]$path, [System.Management.Automation.PSMethod]$shouldDelete, [bool]$recurse) {
+        if (-not (Test-Path $path)) {
+            return 0;
+        }
+        $count = 0
+        $items = if ($recurse) { Get-ChildItem $path -Recurse -Directory } else { Get-ChildItem $path -Directory }
+        foreach ($dir in $items) {
+            if ($shouldDelete.Invoke($dir)) {
+                Write-Host $dir.FullName -NoNewline
+                Write-Host ('.' * (120 - $dir.FullName.Length)) -NoNewline
+                Remove-Item $dir.FullName -Resurse -Force
+                Write-Host ' DELETED' -ForegroundColor Green
+                $count++
+            }
+        }
+        return $count;
+    }
+
+    hidden [bool] shouldAddSlnParameter([ParameterBase[]] $parameters, [hashtable] $config) {
+        return $null -ne $parameters -and
+        $null -ne $config -and $null -ne $config.solutions -and $config.solutions.count -gt 0;
+    }
+
+    hidden [hashtable] fetchSolution([hashtable] $psParams) {
+        $key = $psParams['sln'];
+        if ([string]::IsNullOrEmpty($key)) {
+            return $this.fetchSolutionFromCurrentDir();
+        }
+        $psParams.Remove($key);
+        $result = $this.solutions[$key];
+        if ($null -ne $result) {
+            $result['key'] = $key;
+        }
+        return $result;
+    }
+
+    hidden [hashtable] fetchSolutionFromCurrentDir() {
+        $sln = if ((Test-Path .\src)) {
+            Get-Item .\src\*.sln
+        } else {
+            Get-Item .\*.sln
+        }
+        if ($null -ne $sln) {
+            $slnRoot = $sln.fullname.ToLower();
+            foreach($key in $this.solutions.Keys) {
+                $solution = $this.solutions[$key];
+                if ($slnRoot -eq $solution.path.ToLower()) {
+                    $solution['key'] = $key;
+                    return $solution;
+                }
+            }
+        }
+        return $null;
+    }
 }
